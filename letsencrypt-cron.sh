c65a95c379cdf2a128c967989c0fa73e7e48ee5c
bouroo <bouroo@gmail.com>
2016-04-07 09:50:22 +0700

    Make letsencrypt-auto runable without needing to know the full path
    Signed-off-by: bouroo <bouroo@gmail.com>

--- letsencrypt-cron.sh
+++ letsencrypt-cron.sh
@@ -3,17 +3,23 @@
 # 0 0 1 */2 * /{PATH_TO}/letsencrypt-cron.sh > /var/log/letsencrypt-cron.log
 # Created by @bouroo
 
-# Set path
-LETS_DIR="/root/letsencrypt"
+# Make letsencrypt-auto runable without needing to know the full path
+if [[ ! -e /usr/local/bin/letsencrypt-auto ]]; then
+	cd /usr/local || exit 1
+	if [[ ! -e /usr/local/letsencrypt/letsencrypt-auto ]]; then
+		git clone https://github.com/letsencrypt/letsencrypt.git
+	fi
+	chmod +x /usr/local/letsencrypt/letsencrypt-auto
+	ln -s /usr/local/letsencrypt/letsencrypt-auto /usr/local/bin/letsencrypt-auto
+fi
+
+# Set config path
 CONFIG_FILES=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/conf/*.ini
 
-# Change directory into letsencrypt path
-cd "${LETS_DIR}" || exit 1
-# Pull new letsencrypt
-git pull
+# Run with config in config path
 for ini in $CONFIG_FILES
 do
-./letsencrypt-auto certonly --renew-by-default -c "${ini}"
+letsencrypt-auto certonly --renew-by-default -c "${ini}"
 done
 # Reload web server
 service nginx reload
